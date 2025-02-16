9b297d05352f572fbfed34f5d4196111546ccde5
Lucca <lucca@Luccas-MacBook-Air.local>
2025-02-16 12:19:54 +0100

    draft

--- final_ios/test/widget_test.dart
+++ final_ios/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:final_ios/main.dart';
+import 'package:final_ios/main.old';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
